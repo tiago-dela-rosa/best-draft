eb41b0be6d7a1b7202bda557006f074c5984562b
Tiago Dela Rosa Lopes <tiago.rosa@corebiz.com.br>
2020-01-13 17:16:35 -0300

    Initial commit

---
+++ store/gods.js
@@ -0,0 +1,23 @@
+var gods = [
+	{
+		"name" : "A",
+		"roles" : {
+			"role1" : 50,
+			"role2" : 50,
+			"role3" : 50,
+			"role4" : 50,
+			"role5" : 50
+		}
+
+	},
+	{
+		"name" : "B",
+		"roles" : {
+			"role1" : 50,
+			"role2" : 50,
+			"role3" : 50,
+			"role4" : 50,
+			"role5" : 50
+		}
+	},	
+]
